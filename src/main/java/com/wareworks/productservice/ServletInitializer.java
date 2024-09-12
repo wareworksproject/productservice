d5cc0f6abbdc89d72c5496cdb8bb7c873fded790
Olalekan <amoolekan02@gmail.com>
2024-09-12 22:14:40 +0000

    second commit

--- src/main/java/com/wareworks/productservice/ServletInitializer.java
+++ src/main/java/com/wareworks/productservice/ServletInitializer.java
@@ -3,11 +3,22 @@ package com.wareworks.productservice;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//public class ServletInitializer extends SpringBootServletInitializer {
+
+	//@Override
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		//return application.sources(ProductserviceApplication.class);
+	//}
+
+//}
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ProductserviceApplication.class);
+		return application
+			.sources(ProductserviceApplication.class)
+		        .properties("server.servlet.context-path=/productservice");
 	}
 
 }
